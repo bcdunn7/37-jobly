cf49f6ebe08f06b9602f94974105997d99cbf669
Bryce <bryce.cc.dunn@gmail.com>
2021-08-30 11:57:29 -0500

    pg issue

--- config.js
+++ config.js
@@ -9,11 +9,14 @@ const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 
 const PORT = +process.env.PORT || 3001;
 
+// to deal with invalid characters in password
+const URI_DB_PASSWORD = `${encodeURIComponent(process.env.DB_PASSWORD)}`
+
 // Use dev database, testing database, or via env var, production database
 function getDatabaseUri() {
   return (process.env.NODE_ENV === "test")
-      ? "jobly_test"
-      : process.env.DATABASE_URL || "jobly";
+  ? `postgresql://${process.env.DB_USER}:${URI_DB_PASSWORD}@localhost:${process.env.DB_PORT}/jobly_test`
+  : `postgresql://${process.env.DB_USER}:${URI_DB_PASSWORD}@localhost:${process.env.DB_PORT}/jobly`;
 }
 
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
